a1dcee39a737a206f61ae0eb9a5fa0e4f1dca7b6
Ioannis Krimitzas <i.krimitzas@reply.de>
2022-06-14 23:23:50 +0200

    README

--- tf_modules/simple_ec2/main.tf
+++ tf_modules/simple_ec2/main.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket_object" "object" {
-  #Put the webapp code in an s3 bucket.
+  #Put the webapp code in an s3 bucket as if it was an artifact.
   #Then it will be downloaded by the instance, executed and exposed
   bucket = "sysops-soa-co2-${var.key}" 
   key    = "app.py"
@@ -8,22 +8,16 @@ resource "aws_s3_bucket_object" "object" {
 
 resource "aws_instance" "web" {
   #Create some EC2 instances where we are allowed to connect via SSH.
-  #Iterate over a list and create resources dynamically 
-  for_each = toset(var.instances)
   ami = "ami-0022f774911c1d690"
   key_name = var.key
   user_data_replace_on_change = true
-  vpc_security_group_ids = [
-    aws_security_group.allow_tls.id
-  ]
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
   iam_instance_profile=aws_iam_instance_profile.ec2_ssm_profile.id
   instance_type = "t3.micro"
-#  The SSM agent is preinstalled. Without it, SSM will not be able to execute the necessary actions.
+ #No sudo is needed when using user data commands
   user_data = <<EOF
 #!/bin/bash
 yum install -y jq
-yum install -y https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm
-systemctl start amazon-ssm-agent
 pip3 install flask
 cd home/ec2-user
 mkdir app
@@ -38,18 +32,9 @@ EOF
   }
 }
 
-resource "aws_security_group" "allow_tls" {
-  name        = "allow_tls"
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
   description = "Allow TLS inbound traffic"
-#  SSM will be using port 443 to connect to the instance.
-  ingress {
-    description      = "443 for ssm"
-    from_port        = 443
-    to_port          = 443
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["0.0.0.0/0"]
-  }
 #  We will be using port 22 to connect to the instance.
     ingress {
     description      = "22 for me"
@@ -82,13 +67,14 @@ resource "aws_security_group" "allow_tls" {
   }
 }
 
-resource "aws_iam_instance_profile" "ec2_ssm_profile" {
-  name = "ec2_ssm_profile"
+resource "aws_iam_instance_profile" "ec2_simple_profile" {
+  name = "ec2_simple_profile"
   role = aws_iam_role.role.name
 }
 
+#The role needs EC2 as principal
 resource "aws_iam_role" "role" {
-  name = "ec2_allow_ssm"
+  name = "ec2_simple"
   path = "/"
   assume_role_policy = <<EOF
 {
@@ -107,28 +93,9 @@ resource "aws_iam_role" "role" {
 EOF
 }
 
-data "aws_iam_policy" "CloudWatchFullAccess" {
-  name = "CloudWatchFullAccess"
-}
-
-data "aws_iam_policy" "AmazonSSMManagedInstanceCore" {
-  name = "AmazonSSMManagedInstanceCore"
-}
-
+#Reference existing IAM policy with data
 data "aws_iam_policy" "AmazonS3FullAccess" {
   name = "AmazonS3FullAccess"
-}
-
-#The instance should eventually be able to write logs to Cloudwatch. This policy is good enough
-resource "aws_iam_role_policy_attachment" "CloudWatchFullAccess" {
-  role       = aws_iam_role.role.name
-  policy_arn = data.aws_iam_policy.CloudWatchFullAccess.arn
-}
-
-#The instance should eventually be able to communicate with SSM. This policy is good enough
-resource "aws_iam_role_policy_attachment" "AmazonSSMManagedInstanceCore" {
-  role       = aws_iam_role.role.name
-  policy_arn = data.aws_iam_policy.AmazonSSMManagedInstanceCore.arn
 }
 
 #The instance should be able to download the webapp code from s3. This policy is very wide but good enough
